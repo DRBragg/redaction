54b3b21d092ada65c6d641e0161169a2adee0ccf
Drew Bragg <DRBragg@gmail.com>
2022-03-10 21:44:08 -0500

    Who knows anymore

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[5.1].define(version: 2022_03_09_194044) do
+ActiveRecord::Schema.define(version: 2022_03_09_194044) do
   create_table "comments", force: :cascade do |t|
     t.integer "post_id", null: false
     t.integer "user_id", null: false
