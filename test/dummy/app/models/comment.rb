ab085519b45507cf6fcaeb3e527570a07c0440be
Drew Bragg <drbragg@gmail.com>
2022-03-12 12:00:15 -0500

    Update tests

--- test/dummy/app/models/comment.rb
+++ test/dummy/app/models/comment.rb
@@ -2,6 +2,21 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
 
+  after_save :update_content
+  after_save :update_subject, unless: :redacting?
+
   redacts :content, with: :text
   redacts :subject
+
+  validates :subject, length: { maximum: 10 }
+
+  private
+
+  def update_content
+    update_column(:content, content + " Updated in Callback")
+  end
+
+  def update_subject
+    update_column(:subject, subject + " Updated in Callback")
+  end
 end
