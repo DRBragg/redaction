c88a10aa6332fd93ede64d887f07c022141e4a45
Drew Bragg <drbragg@gmail.com>
2022-04-01 12:48:58 -0400

    Correct schema

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_01_163109) do
+ActiveRecord::Schema.define(version: 2022_04_01_163109) do
   create_table "accounts", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "phone_number"
